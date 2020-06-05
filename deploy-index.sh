36b97b51a7a730cc28ebcf5f353730d4bf5174c7
Jarek Gawor <jgawor@gmail.com>
2020-06-05 16:50:00 -0400

    appsody info

--- deploy-index.sh
+++ deploy-index.sh
@@ -20,3 +20,13 @@ sed -i -e "s|NAMESPACE|$IMAGE_ORG|" $YAML_FILE
 sed -i -e 's|TAG|latest|' $YAML_FILE
 sed -i -e "s|DATE|$(date --utc '+%FT%TZ')|" $YAML_FILE
 oc -n kabanero apply --validate=false -f $YAML_FILE
+
+ROUTE=$(oc get route kabanero-index --no-headers -o=jsonpath='{.status.ingress[0].host}' -n kabanero)
+STACK_HUB_URL="http://$ROUTE/kabanero-stack-hub-index.yaml"
+
+echo
+echo "Stack Hub Index: $STACK_HUB_URL"
+echo
+echo "You can run the following command to configure appsody:"
+echo "  appsody repo add cp4apps $STACK_HUB_URL"
+echo "  appsody list cp4apps"
